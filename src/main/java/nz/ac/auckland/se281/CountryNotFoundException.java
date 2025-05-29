9c7e6d3b24b05936ae11e665955f934c40006e7a
Hargun Singh <hargunsingh@Harguns-MacBook-Air.local>
2025-05-29 22:54:26 +1200

    Added a javadoc for public methods

--- src/main/java/nz/ac/auckland/se281/CountryNotFoundException.java
+++ src/main/java/nz/ac/auckland/se281/CountryNotFoundException.java
@@ -1,7 +1,29 @@
 package nz.ac.auckland.se281;
 
+/**
+ * Custom exception thrown when a requested country cannot be found. This exception is used to
+ * handle invalid country names entered by the user.
+ */
 public class CountryNotFoundException extends Exception {
+
+  private String countryName;
+
+  /**
+   * Constructs a new exception with a message indicating that the country was not found.
+   *
+   * @param countryName The name of the country that could not be found
+   */
   public CountryNotFoundException(String countryName) {
     super(MessageCli.INVALID_COUNTRY.getMessage(countryName));
+    this.countryName = countryName;
+  }
+
+  /**
+   * Gets the name of the country that was not found.
+   *
+   * @return The country name
+   */
+  public String getCountryName() {
+    return countryName;
   }
 }
